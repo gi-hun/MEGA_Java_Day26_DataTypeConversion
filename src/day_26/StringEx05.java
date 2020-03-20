df5ce53c7f22f29ae0b033d0c92a7aac8fbd07f8
duarlgns1822@gmail.com <duarl@DESKTOP-RLI1BQT>
2020-03-20 23:25:04 +0900

    # 형변환(type conversion)

---
+++ src/day_26/StringEx05.java
@@ -0,0 +1,31 @@
+/*
+ * # 형변환(type conversion)
+ * 1) 자료형(data type)을 바꾸는 것을 의미
+ * 2) (변환할 자료형)변수
+ */
+package day_26;
+
+public class StringEx05 {
+	public static void main(String[] args) {
+		//문자-> 숫자
+		char ch = 'a';
+		int aNum = (int)ch;
+		System.out.println(aNum);		//97
+		
+		ch = (char)(aNum  + 1);
+		System.out.println(ch); 		//b
+		
+		//문자열-> 숫자
+		String strNum = "10";
+		int num = Integer.parseInt(strNum);
+		System.out.println(num+1);
+		
+		//숫자-> 문자열
+		//방법1) 꼼수
+		strNum = num + "";
+		System.out.println(strNum + 1);		//101
+		//방법2) 정식
+		strNum = String.valueOf(num);
+		System.out.println(strNum + 1);		//101
+	}
+}
